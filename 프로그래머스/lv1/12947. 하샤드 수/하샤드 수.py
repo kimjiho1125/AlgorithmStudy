3ddf21e43bf26de701fe38f824c79ff43d10f5ff
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-03-29 18:15:25 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,10 +1,2 @@
 def solution(x):
-    x = str(x)
-    sum = 0
-    for i in x:
-        sum += int(i)
-    if(int(x) % sum == 0):
-        answer = True
-    else:
-        answer = False
-    return answer
+    return x%sum(int(x) for x in str(x)) == 0
