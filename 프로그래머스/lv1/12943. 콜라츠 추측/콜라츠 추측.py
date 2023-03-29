8ca0513d529a03d78bdd4f2b544b4527511496f2
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-03-29 18:53:36 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,16 +1,9 @@
 def solution(num):
-    count = 0
     if(num == 1):
         return 0
-    while count <= 500:
-        if(num % 2 != 0):
-            num = num * 3 + 1
-            count += 1
-        if(num % 2 == 0):
-            num /= 2
-            count += 1
-        if(count == 500):
-            return -1
-        if(num == 1):
-            return count
-    
+    else:
+        for i in range(500):
+            num = num / 2 if num % 2 == 0 else num*3+1
+            if(num == 1):
+                return i+1
+        return -1
