1de71638d548ab66e790fb973a50516e968236b0
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-03-31 19:57:26 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,10 @@
 def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(absolutes)):
-        if(signs[i]):
-            answer += absolutes[i]
-        else:
-            answer -= absolutes[i]
-    return answer
+    # answer = 0
+    # for i in range(len(absolutes)):
+    #     if(signs[i]):
+    #         answer += absolutes[i]
+    #     else:
+    #         answer -= absolutes[i]
+    # return answer
+    # 더 좋은 풀이
+    return sum(absolute if sign else -absolute for absolute,sign in zip(absolutes,signs))
