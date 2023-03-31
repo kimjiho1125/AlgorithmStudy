7b1130f567ee77a9b3c20398a009bc56e238ddee
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-03-31 20:13:36 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,7 +1,9 @@
 def solution(numbers):
-    condition = [0,1,2,3,4,5,6,7,8,9]
-    for i in numbers:
-        condition.remove(i)
-    return sum(condition)
+    # condition = [0,1,2,3,4,5,6,7,8,9]
+    # for i in numbers:
+    #     condition.remove(i)
+    # return sum(condition)
+    # 더 간단한 코드
+    return 45 - sum(numbers)
         
     
