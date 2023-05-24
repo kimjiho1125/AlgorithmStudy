bfcdd5109c4f51771024351927037345c60a5fdf
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-05-24 14:47:19 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,21 @@
+def solution(wallpaper):
+    lux = 100
+    luy = 100
+    rdx = 0
+    rdy = 0
+    
+    for i, items in enumerate(wallpaper):
+        for j, item in enumerate(items):
+            if item == "#":
+                if lux > i:
+                    lux = i
+                if luy > j:
+                    luy = j
+                if rdx < i+1:
+                    rdx = i+1
+                if rdy < j+1:
+                    rdy = j+1
+    
+    return [lux,luy,rdx,rdy] 
+    
+    
