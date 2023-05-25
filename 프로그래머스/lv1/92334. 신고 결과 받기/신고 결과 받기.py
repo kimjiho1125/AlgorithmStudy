787023ce1f034826631482bf34c2d7c99ecea5ca
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-05-25 15:07:01 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,26 +1,37 @@
 def solution(id_list, report, k):
-    answer = []
-    reportedCount = dict()
-    user_info = dict()
-    reportedId = []
+    answer = [0] * len(id_list)
+    reports = {x: 0 for x in id_list}
     
-    for i in id_list:
-        user_info[i] = []
-        reportedCount[i] = 0
+    for r in set(report):
+        reports[r.split()[1]] += 1
     
-    for case in report:
-        reporter, reported = case.split(' ')
-        if reported not in user_info[reporter]:
-            user_info[reporter].append(reported)
-            reportedCount[reported] += 1
-            if reportedCount[reported] == k:
-                reportedId.append(reported)
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
     
-    for value in user_info.values():
-        count = 0
-        for i in value:
-            if i in reportedId:
-                count += 1
-        answer.append(count)
-        
     return answer
+#     answer = []
+#     reportedCount = dict()
+#     user_info = dict()
+#     reportedId = []
+    
+#     for i in id_list:
+#         user_info[i] = []
+#         reportedCount[i] = 0
+    
+#     for case in report:
+#         reporter, reported = case.split(' ')
+#         if reported not in user_info[reporter]:
+#             user_info[reporter].append(reported)
+#             reportedCount[reported] += 1
+#             if reportedCount[reported] == k:
+#                 reportedId.append(reported)
+    
+#     for value in user_info.values():
+#         count = 0
+#         for i in value:
+#             if i in reportedId:
+#                 count += 1
+#         answer.append(count)
+        
+#     return answer
