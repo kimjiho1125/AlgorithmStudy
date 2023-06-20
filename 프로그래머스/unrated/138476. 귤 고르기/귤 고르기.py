07524415f4b6cfaa4dcb17761a8413a0295a44fc
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-06-20 15:42:35 +0900

    [unrated] Title: 귤 고르기, Time: 107.09 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,29 +1,8 @@
+from collections import Counter
+
 def solution(k, tangerine):
-    dict_type = {}
-    answer = 0
-    tangerine.sort()
-    prevNum = tangerine[0]
-    currentNum = 0
-    # O(N) 복잡도로 각 크기별 개수 정보를 저장하고 있는 딕셔너리 생성
-    for i, num in enumerate(tangerine):
-        currentNum = num
-        if i == 0:
-            dict_type[num] = 1
-            prevNum = num
-        else:
-            if prevNum == currentNum:
-                dict_type[currentNum] += 1
-            elif prevNum != currentNum:
-                dict_type[currentNum] = 1
-                prevNum = currentNum
-    # 딕셔너리를 value값을 기준으로 내림차순으로 정렬
-    dict_type = sorted(dict_type.items(), key = lambda item:item[1], reverse = True)
-   
-    # 정렬된 딕셔너리를 반복문을 통해 돌면서 k개를 달성하면 크기가 몇 종류인지 리턴해준다
-    for value in dict_type:
-        answer += 1
-        k -= value[1]
-        if k <= 0:
-            return answer
+    counter = Counter(tangerine)
+    tangerine.sort(key = lambda t: (-counter[t],t))
+    return len(set(tangerine[:k]))
     
     
