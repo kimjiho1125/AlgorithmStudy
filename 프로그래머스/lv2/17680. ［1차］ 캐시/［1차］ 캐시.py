1fda78adfc8f03fe692b4ff85d871bf410625b86
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-07-04 15:04:02 +0900

    [level 2] Title: [1차] 캐시, Time: 0.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -3,6 +3,7 @@ from collections import deque
 def solution(cacheSize, cities):
     time = 0
     cache = deque(maxlen=cacheSize)
+    #maxlen 설정시 maxlen 이상의 값을 추가할 경우 맨 왼쪽 원소가 삭제되고, 값이      들어간다! 
     
     for city in cities:
         #대소문자 구분 X
@@ -10,6 +11,7 @@ def solution(cacheSize, cities):
 
         if city in cache:
             time += 1
+            #LRU 알고리즘에 의해 캐시 순서 최신화
             cache.remove(city)
             cache.append(city)
         else:
