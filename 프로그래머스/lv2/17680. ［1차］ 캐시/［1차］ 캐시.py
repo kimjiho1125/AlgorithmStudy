f21c7e5f289997383497557d1c4e2bb126158b34
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-07-04 15:02:52 +0900

    [level 2] Title: [1차] 캐시, Time: 0.72 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,30 +1,19 @@
 from collections import deque
 
 def solution(cacheSize, cities):
-    answer = 0
-    #빈 deque 만들기
-    cache = deque()
-    if cacheSize > 0:
-        for city in cities:
-            #대소문자 구분 X
-            city = city.lower()
+    time = 0
+    cache = deque(maxlen=cacheSize)
+    
+    for city in cities:
+        #대소문자 구분 X
+        city = city.lower()
 
-            if len(cache) > 0:
-                if city in cache:
-                    answer += 1
-                    cache.remove(city)
-                    cache.append(city)
-                elif city not in cache:
-                    answer += 5
-                    if len(cache) == cacheSize:
-                        cache.popleft()
-                        cache.append(city)
-                    elif len(cache) < cacheSize:
-                        cache.append(city)
-            elif len(cache) == 0:
-                answer += 5
-                cache.append(city)
-    elif cacheSize == 0:
-        answer = len(cities) * 5
-            
-    return answer
+        if city in cache:
+            time += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            time +=5
+            cache.append(city)
+                    
+    return time
