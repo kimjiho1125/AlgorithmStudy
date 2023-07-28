9cd38f647b657f0594f28372249c6b98f59af19b
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-07-28 23:39:34 +0900

    [level 2] Title: 오픈채팅방, Time: 113.65 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,28 @@
+def solution(record):
+    result = []
+    #uid의 nickname 데이터를 저장할 딕셔너리를 생성한다
+    data = dict()
+    
+    #record를 순회하며 최종적인 record를 만든다
+    for i in range(len(record)):
+        if record[i][0:6] == "Change":
+            situation, uid, nickname = record[i].split()
+            data[uid] = nickname
+        else:
+            if record[i][0:5] == "Enter":
+                situation, uid, nickname = record[i].split()
+                data[uid] = nickname
+    #결과 메시지를 result 배열에 추가한다
+    for i in range(len(record)):
+        if record[i][0:6] == "Change":
+            continue
+        elif record[i][0:5] == "Enter":
+            situation, uid, nickname = record[i].split()
+            temp = data[uid] + "님이 " + "들어왔습니다."
+            result.append(temp)
+        elif record[i][0:5] == "Leave":
+            situation, uid = record[i].split()
+            temp = data[uid] + "님이 " + "나갔습니다."
+            result.append(temp)
+
+    return result
