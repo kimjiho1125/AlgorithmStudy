203bbf84f136ffc1574b93f57aad70ffa49bd45c
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-08-10 20:02:03 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,21 +1,15 @@
 def solution(skill, skill_trees):
     answer = 0
-    skills = []
-    #스킬트리에 있는 스킬들을 순서대로 리스트에 저장
-    for s in skill:
-        skills.append(s)
-        
+    
     for skill_tree in skill_trees:
-        order = 0
-        flag = True
+        skill_list = list(skill)
+        
         for s in skill_tree:
-            if s in skills:
-                if s == skills[order]:
-                    order += 1
-                else:
-                    flag = False
+            if s in skill_list:
+                if s != skill_list.pop(0):
                     break
-        if flag:
+        else:
+            print(skill_tree)
             answer += 1
             
     return answer
