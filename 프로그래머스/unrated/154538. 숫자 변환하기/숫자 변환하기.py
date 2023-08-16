6a5092f5a00fe55f41dd00edbb1b0b45c1867ad5
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-08-16 22:06:48 +0900

    [unrated] Title: 숫자 변환하기, Time: 26.64 ms, Memory: 17.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def bfs(x, y, n, dist):
+    queue = deque()
+    dist[x] = 1
+    queue.append(x)
+    
+    while queue:
+        current = queue.popleft()
+        
+        if 0 <= current + n <= 1000000 and dist[current + n] == 0:
+            dist[current + n] = dist[current] + 1
+            queue.append(current + n)
+        if 0 <= current * 2 <= 1000000 and dist[current * 2] == 0:
+            dist[current * 2] = dist[current] + 1
+            queue.append(current * 2)
+        if 0 <= current * 3 <= 1000000 and dist[current * 3] == 0:
+            dist[current * 3] = dist[current] + 1
+            queue.append(current * 3)
+        
+def solution(x, y, n):
+    dist = [0] * 1000001
+    bfs(x,y,n,dist)
+    return dist[y] - 1
