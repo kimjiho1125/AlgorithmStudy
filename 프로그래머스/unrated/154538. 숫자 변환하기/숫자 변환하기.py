d02d06b2a6df413bde48320bfcdc3254ea355371
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-08-16 22:52:59 +0900

    [unrated] Title: 숫자 변환하기, Time: 70.90 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,25 +1,19 @@
-from collections import deque
-
-def dp(x, y, n, dist):
-    queue = deque()
-    dist[x] = 1
-    queue.append(x)
-    
-    while queue:
-        current = queue.popleft()
-        
-        if 0 <= current + n <= 1000000 and dist[current + n] == 0:
-            dist[current + n] = dist[current] + 1
-            queue.append(current + n)
-        if 0 <= current * 2 <= 1000000 and dist[current * 2] == 0:
-            dist[current * 2] = dist[current] + 1
-            queue.append(current * 2)
-        if 0 <= current * 3 <= 1000000 and dist[current * 3] == 0:
-            dist[current * 3] = dist[current] + 1
-            queue.append(current * 3)
-        
 def solution(x, y, n):
-    # +1을 한 이유는 직관적으로 인덱스와 값을 일치시키기 위해서
-    dist = [0] * 1000001
-    dp(x,y,n,dist)
-    return dist[y] - 1
+    # y+1인 이유는 직관적으로 인덱스와 값을 일치시키기 위해서임
+    memorized = [1e9] * (y+1)
+    memorized[x] = 0
+    
+    for i in range(x,y+1):
+        if memorized[i] == 1e9:
+            continue
+        if i + n <= y:
+            memorized[i+n] = min(memorized[i+n],memorized[i] + 1)
+        if i * 2 <= y:
+            memorized[i*2] = min(memorized[i*2],memorized[i] + 1)
+        if i * 3 <= y:
+            memorized[i*3] = min(memorized[i*3],memorized[i] + 1)
+    
+    if memorized[y] == 1e9:
+        return -1
+    
+    return memorized[y] 
