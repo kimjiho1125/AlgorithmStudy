d1a9508cc24ab521ae48cd3a2285a699320b6f32
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-09-01 22:19:51 +0900

    [level 2] Title: 삼각 달팽이, Time: 316.44 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,38 +1,32 @@
 def solution(n):
     result = []
+    #배열 초기화
     field = [[0 for i in range(n)] for j in range(n)]
-    
     r,c = 0,0
     field[r][c] = 1
     
     currentNum = 1
+    #등차수열 합공식
     finalNum = n*(n+1) / 2
     
+    #종료 조건 - currentNum가 finalNum와 같아질 때
     while currentNum < finalNum:
-        endCheck = 0
         
         while r+1 <= n-1 and field[r+1][c] == 0:
             currentNum += 1
             r,c = r+1,c
             field[r][c] = currentNum
-            endCheck += 1
         
         while c+1 <= n-1 and field[r][c+1] == 0:
             currentNum += 1
             r,c = r,c+1
             field[r][c] = currentNum
-            endCheck += 1
         
         while c-1 >= 0 and r-1 >= 0 and field[r-1][c-1] == 0:
             currentNum += 1
             r,c = r-1, c-1
             field[r][c] = currentNum
-            endCheck += 1
             
-        #종료 조건 - 위의 세 가지 경우 어디에도 해당하지 않는 경우
-        if endCheck == 0:
-            break
-    
     #0을 제거하고 최종 결과 리스트로 만들기
     for i in range(n):
         for j in range(n):
