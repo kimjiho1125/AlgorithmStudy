b1982988c9929634929ed3bcb8487425eb28f185
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-10-25 16:24:06 +0900

    [level 2] Title: 하노이의 탑, Time: 12.12 ms, Memory: 18.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = hanoi(n,1,3,2)
+    return answer
+
+def hanoi(n,start,end,bypass):
+    if n==1:
+        return [[start, end]]
+    else:
+        path = []
+        path += hanoi(n-1,start,bypass,end)
+        path += [[start,end]]
+        path += hanoi(n-1,bypass,end,start)
+              
+        return path
