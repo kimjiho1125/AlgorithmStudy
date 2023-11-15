7941cda1f9261e97cd6fabf01673ca9a3b7ac3c3
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2023-11-15 22:52:56 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def bfs(place):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    #시작점이 될 P의 리스트를 저장할 변수
+    start = []
+    #P의 좌표 구하기
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == 'P':
+                start.append([i,j])
+    for s in start:
+        queue = deque([s])
+        visited = [[0]*5 for _ in range(5)]
+        distance = [[0]*5 for _ in range(5)]
+        visited[s[0]][s[1]] = 1
+        
+        while queue:
+            y,x = queue.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if 0<=nx<5 and 0<=ny<5 and visited[ny][nx] == 0:
+                    
+                    if place[ny][nx] == 'O':
+                        queue.append([ny,nx])
+                        visited[ny][nx] = 1
+                        distance[ny][nx] = distance[y][x] + 1
+                    
+                    if place[ny][nx] == 'P' and distance[y][x] <= 1:
+                        return 0
+    
+    return 1
+                
+def solution(places):
+    answer = []
+    for place in places:
+        answer.append(bfs(place))
+    
+    return answer
