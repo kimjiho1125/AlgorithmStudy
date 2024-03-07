1fc0c0f9606aea70e92c4198f4a1cff2268a50ec
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-03-07 20:47:19 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def solution(cards):
+    answer = []
+    for i in range(len(cards)):
+        tmp = 0
+        while cards[i]:
+            nextIdx = cards[i] - 1
+            cards[i] = 0
+            i = nextIdx
+            tmp += 1
+        answer.append(tmp)
+    answer.sort()
+    
+    return answer[-1] * answer[-2]
