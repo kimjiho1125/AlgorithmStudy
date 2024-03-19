b6bb2a27018ddff34c91ea3c48a3a3ce6a610fb8
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-03-19 23:45:51 +0900

    [Silver I] Title: 안전 영역, Time: 844 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,48 +1,52 @@
+import sys
 from collections import deque
- 
-n = int(input())
-graph = []
-maxNum = 0
- 
+
+n = int(sys.stdin.readline())
+board = []
+max_len = 0
+result = 0
+
+#board 초기화 및 최대높이 찾기
 for i in range(n):
-    graph.append(list(map(int, input().split())))
+    board.append(list(map(int, sys.stdin.readline().split())))
     for j in range(n):
-        if graph[i][j] > maxNum:
-            maxNum = graph[i][j] 
- 
- 
- 
-dx = [0 ,0, 1, -1]
-dy = [1, -1, 0 ,0]
-def bfs(a, b, value, visited):
-    q = deque()
-    q.append((a, b))
-    visited[a][b] = 1
- 
-    while q:
-        x, y = q.popleft()
- 
+        if board[i][j] > max_len:
+            max_len = board[i][j]
+
+dy = [-1,0,1,0]
+dx = [0,1,0,-1]
+
+def bfs(x,y,rain_length):
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = True
+
+    while queue:
+        x,y = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] > value and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    q.append((nx, ny))
- 
- 
-result = 0
-for i in range(maxNum): 
-    visited = [[0] * n for i in range(n)]
-    cnt = 0
- 
-    for j in range(n):
-        for k in range(n):
-            if graph[j][k] > i and visited[j][k] == 0: 
-                bfs(j, k, i, visited)
-                cnt += 1
- 
-    if result < cnt:
-        result = cnt
- 
+
+            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] > rain_length and not visited[nx][ny]:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+
+
+#높이가 1일때부터 100일때까지 모든 경우의 수 구하기
+for rain_length in range(max_len):
+    visited = [[False for _ in range(n)] for _ in range(n)] #방문여부
+    count = 0 #안전 지대 수
+
+    #안전지대 체크
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > rain_length and not visited[i][j]:
+                bfs(i,j,rain_length)
+                count += 1
+
+    #안전지대 수 업데이트
+    result = count if count > result else result
+
+
 print(result)
