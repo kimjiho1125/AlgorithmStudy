e0f1682e29c040cb27dfe762ec60c6292abd8cd1
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-03-22 22:58:35 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,20 @@
+def solution(n, s):
+    answer = []
+    
+    if n > s:
+        return [-1]
+    
+    num, rest = divmod(s,n)
+    
+    for _ in range(n):
+        answer.append(num)
+    
+    if rest > 0:
+        for i in range(n-1,-1,-1):
+            answer[i] += 1
+            rest -= 1
+            #종료 조건
+            if rest == 0:
+                break
+    
+    return answer
