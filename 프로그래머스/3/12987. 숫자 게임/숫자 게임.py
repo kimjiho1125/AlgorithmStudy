628ec1b27f7a6c29ecd1ced3dbedeafb5ec5c5a8
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-03-25 21:18:29 +0900

    [level 3] Title: 숫자 게임, Time: 39.58 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -2,11 +2,11 @@ def solution(A, B):
     answer = 0
     A.sort()
     B.sort()
-    for n in A:
-        for i in range(len(B)):
-            if n < B[i]:
-                B.pop(i)
-                answer += 1
-                break
-                
+    j = 0
+    
+    for i in range(len(A)):
+        if A[j] < B[i]:
+            answer += 1
+            j += 1
+            
     return answer
