85eedfafe740549fa3f0f2334b47f3ed79a9d726
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-10-11 15:06:45 +0900

    [Silver IV] Title: 대피소, Time: 340 ms, Memory: 112612 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -4,9 +4,7 @@ from itertools import combinations
 input = sys.stdin.readline
 INF = 1e9
 
-
 N, K = map(int, input().split())
-
 #집들에 대한 정보
 houses = [list(map(int, input().split())) for _ in range(N)]
 #모든 가능한 대피소의 조합
@@ -18,6 +16,7 @@ result = INF
 def cal_distance(p1,p2):
   return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
+#모든 집이 쉘터인 경우 예외처리
 if N == K:
   print(0)
 
@@ -30,7 +29,8 @@ else:
         #해당 집에서 가장 가까운 쉘터까지의 거리 구하기
         for s in comb:
           c_distance = min(c_distance, cal_distance(s, house))
-        #가장 가까운 쉘터까지의 거리 중 가장 큰 값 구하기
+        #가장 가까운 쉘터까지의 거리 중 가장 큰 값 찾기
         local_max = max(local_max, c_distance)
+    #local_max 최소가 되는 값 찾기
     result = min(result, local_max)
   print(result)
